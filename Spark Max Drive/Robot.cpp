c3d9dd51c30e38fb87bd76bed29d72dd65c0c307
Aidan <133599062+DoublePain@users.noreply.github.com>
2024-11-12 12:55:48 -0600

    Add files via upload

---
+++ Spark Max Drive/Robot.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+#include <fmt/core.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {
+//Print out the motor ID'S for each of your motors if you arent sure which is which! :)
+std::cout << "Left Front Motor ID: " << LeftFront.GetChannel() << std::endl;
+std::cout << "Right Front Motor ID: " << RightFront.GetChannel() << std::endl;
+std::cout << "Left Back Motor ID: " << LeftBack.GetChannel() << std::endl;
+std::cout << "Right Back Motor ID: " << RightBack.GetChannel() << std::endl;
+std::cout << "Right Back Motor ID: " << IntakeMotor.GetChannel() << std::endl;
+
+}
+
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {
+ 
+}
+
+void Robot::AutonomousPeriodic() {
+
+}
+
+void Robot::TeleopInit() {}
+
+//Once teleop begins, everything in here will run periodically
+void Robot::TeleopPeriodic() {
+  arcade_drive(); //Calling the drive function
+  Intake(); // Calling the intake function
+}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+
+void Robot::SimulationPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ Spark Max Drive/Robot.h
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/Joystick.h>
+#include <frc/TimedRobot.h>
+#include <frc/Timer.h>
+#include <string>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <iostream> 
+#include <frc/motorcontrol/PWMSparkMax.h>
+
+using namespace frc;
+
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+  void arcade_drive(void);
+  void Intake(void) ;
+  void Auto(double);
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+//Joystick
+  Joystick Controller{0};
+
+//Timer for auto
+Timer AutoTimer;
+
+//Motor controllers
+//The motor ID's will likely need to be changed in accordance with your robot wiring to the pdp
+PWMSparkMax LeftFront{0};
+PWMSparkMax RightFront{1};
+PWMSparkMax RightBack{2};
+PWMSparkMax LeftBack{3};
+PWMSparkMax IntakeMotor{4};
+
+};
